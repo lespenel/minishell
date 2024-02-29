c9c4e8a9e787ea7d74fbaa47040bb6196a397f7a
lespenel <lespenel@student.42lyon.fr>
2024-03-01 00:29:37 +0100

    clear lexer from main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:52:26 by lespenel          #+#    #+#             */
-/*   Updated: 2024/02/28 23:54:37 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/03/01 00:28:58 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,15 @@ int	main(int argc, char **argv, char **envp)
 {
 	const char	*prompt = "minishell $> ";
 	char		*str;
-	t_lexer		token_tab;
 
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	init_lexer(&token_tab);
 	str = readline(prompt);
 	if (str == NULL)
 		return (1);
 	while (str)
 	{
-		init_lexer(&token_tab);
-		if (fill_lexer(&token_tab, str) == -1)
-			return (printf("Error lexer\n"));
-		print_lexer(&token_tab);
-		clear_lexer(&token_tab);
 		free(str);
 		str = readline(prompt);
 	}

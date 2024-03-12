35cb5f2d7042a7d35183c45c7bd476fdadab6217
lespenel <lespenel@student.42lyon.fr>
2024-03-12 02:00:17 +0000

    New 'add_vector_at' function in vector lib + fixed function name 'ft_remove_vector' to 'remove_vector' accordingly to the vector.h file

---
+++ lib/libft/src/vector/add_vector_at.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_vector_at.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 01:47:35 by lespenel          #+#    #+#             */
+/*   Updated: 2024/03/12 01:57:53 by lespenel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vector.h"
+#include "ft_mem.h"
+
+int	add_vector_at(t_vector *this, const void *new, size_t at)
+{
+	size_t	i;
+
+	i = 0;
+	if (at > this->size)
+		return (-1);
+	if (this->size + 1 >= this->allocated)
+	{
+		set_alloc_size(this, 1);
+		if (reallocate_vector(this))
+			return (-1);
+	}
+	while (i < (this->size - at))
+	{
+		ft_memcpy(this->array + (this->size - i) * this->elemsize, this->array
+			+ (this->size - i), this->elemsize);
+		++i;
+	}
+	ft_memcpy(this->array + (at * this->elemsize), new, this->elemsize);
+	this->size += 1;
+	return (0);
+}
--- lib/libft/src/vector/remove_vector.c
+++ lib/libft/src/vector/remove_vector.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_remove_vector.c                                 :+:      :+:    :+:   */
+/*   remove_vector.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 10:17:00 by ccouble           #+#    #+#             */
-/*   Updated: 2024/02/28 01:09:57 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/03/12 01:59:18 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
 #include "ft_mem.h"
 
-void	ft_remove_vector(t_vector *this, size_t i)
+void	remove_vector(t_vector *this, size_t i)
 {
 	const size_t	rmi = i * this->elemsize;
 	const size_t	sizei = (this->size - 1) * this->elemsize;

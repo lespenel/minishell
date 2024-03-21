17a37870fb55e8b0fa7319b66a8ffdb48fa48648
ccouble <ccouble@student.42lyon.fr>
2024-03-21 05:34:42 +0100

    vector automatically put empty elemsize next to elem all the time

--- lib/libft/src/vector/set_alloc_size.c
+++ lib/libft/src/vector/set_alloc_size.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_set_alloc_size.c                                :+:      :+:    :+:   */
+/*   set_alloc_size.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 10:01:45 by ccouble           #+#    #+#             */
-/*   Updated: 2024/02/28 01:09:51 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/03/21 05:33:54 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	set_alloc_size(t_vector *this, size_t count)
 {
-	if (this->size + count >= this->allocated)
+	if (this->allocated == 0 || this->size + count >= this->allocated - 1)
 	{
 		if (this->allocated == 0)
 			this->allocated = BASE_ALLOC;

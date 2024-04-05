8a744e6e1f8c0b6fd07c9615c1568a98a2cea5c3
lespenel <leoespenel@hotmail.com>
2024-04-05 03:53:53 +0200

    corrected onanisme with the allocated ptr in the strtok loop for globinore

--- src/wildcard/pattern/create_glob_pattern.c
+++ src/wildcard/pattern/create_glob_pattern.c
@@ -6,14 +6,17 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 02:45:28 by lespenel          #+#    #+#             */
-/*   Updated: 2024/03/26 07:20:03 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/04/05 03:50:47 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
+#include "lexer.h"
 #include "util.h"
 #include "wildcard.h"
 #include <stdlib.h>
+
+static	int	fill_glob_pattern(t_wild *wild, char *entry, t_pattern *pattern);
 
 int	create_glob_pattern(t_wild *wild)
 {
@@ -25,21 +28,31 @@ int	create_glob_pattern(t_wild *wild)
 	entry = ft_strdup(wild->globignore);
 	if (entry == NULL)
 		return (-1);
-	entry = ft_strtok(entry, ":");
-	while (entry)
+	if (fill_glob_pattern(wild, entry, &pattern) == -1)
 	{
-		if (ms_strchr(entry, '/'))
-			pattern.type = FILENAME;
-		else
-			pattern.type = NO_STAR;
-		if (fill_pattern(&pattern.pattern, entry) == -1
-			|| add_vector(&wild->glob_patterns, &pattern, 1) == -1)
-		{
-			free(entry);
-			return (-1);
-		}
-		entry = ft_strtok(NULL, ":");
+		clear_lexer(&pattern.pattern);
+		free(entry);
+		return (-1);
 	}
 	free(entry);
+	return (0);
+}
+
+static	int	fill_glob_pattern(t_wild *wild, char *entry, t_pattern *pattern)
+{
+	char	*tok;
+
+	tok = ft_strtok(entry, ":");
+	while (tok)
+	{
+		if (ms_strchr(tok, '/'))
+			pattern->type = FILENAME;
+		else
+			pattern->type = NO_STAR;
+		if (fill_pattern(&pattern->pattern, tok) == -1
+			|| add_vector(&wild->glob_patterns, pattern, 1) == -1)
+			return (-1);
+		tok = ft_strtok(NULL, ":");
+	}
 	return (0);
 }

47d5a27346d8ade12cdb87a3104d8741357dbf6c
lespenel <leoespenel@hotmail.com>
2024-04-05 02:01:37 +0200

    newline tok is no longerd mallocd

--- src/lexer/add_newline_tok.c
+++ src/lexer/add_newline_tok.c
@@ -6,21 +6,18 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 00:11:05 by lespenel          #+#    #+#             */
-/*   Updated: 2024/03/12 00:11:05 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/04/05 02:01:17 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
-#include "ft_string.h"
 #include "vector.h"
 
 int	add_newline_tok(t_lexer *lexer)
 {
 	t_lexer_tok	token;
 
-	token.content = ft_strdup("newline");
-	if (token.content == NULL)
-		return (-1);
+	token.content = NULL;
 	token.type = NEWLINE;
 	if (add_vector(lexer, &token, 1) == -1)
 		return (-1);

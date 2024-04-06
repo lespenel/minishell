2ce08a276babe506e3aa43b23806f9c106e70c3a
ccouble <ccouble@student.42lyon.fr>
2024-04-06 23:15:28 +0200

    added $?

--- src/destroy_minishell.c
+++ src/destroy_minishell.c
@@ -6,13 +6,19 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:52:42 by ccouble           #+#    #+#             */
-/*   Updated: 2024/04/04 13:52:48 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/04/06 23:06:08 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
 
 void	destroy_minishell(t_ms *ms)
 {
 	destroy_env(&ms->env);
+	if (ms->lastexitstr)
+	{
+		free(ms->lastexitstr);
+		ms->lastexitstr = NULL;
+	}
 }
--- src/util/set_exitcode_str.c
+++ src/util/set_exitcode_str.c
@@ -1,31 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   set_exitcode_str.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/03 03:19:20 by ccouble           #+#    #+#             */
-/*   Updated: 2024/04/06 23:01:24 by ccouble          ###   ########.fr       */
+/*   Created: 2024/04/06 23:01:51 by ccouble           #+#    #+#             */
+/*   Updated: 2024/04/06 23:03:34 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
+#include "ft_string.h"
+#include <stdlib.h>
 
-# define PROMPT "minishell $> "
-
-# include "env.h"
-# include "lexer.h"
-
-typedef struct s_ms
+int	set_exitcode_str(t_ms *ms, int exitcode)
 {
-	t_env	env;
-	int		lastexit;
-	char	*lastexitstr;
-}	t_ms;
-
-void	destroy_minishell(t_ms *ms);
-int		parse_input(t_ms *ms, t_lexer *lexer, char *input);
-
-#endif
+	ms->lastexit = exitcode;
+	if (ms->lastexitstr)
+	{
+		free(ms->lastexitstr);
+		ms->lastexitstr = NULL;
+	}
+	ms->lastexitstr = ft_itoa(exitcode);
+	if (ms->lastexitstr == NULL)
+		return (-1);
+	return (0);
+}

1c45831d3aa325a451e4ba71ae50a7a51b479357
ccouble <ccouble@student.42lyon.fr>
2024-04-07 05:40:16 +0200

    make project compile

--- include/util.h
+++ include/util.h
@@ -6,15 +6,16 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:46:36 by lespenel          #+#    #+#             */
-/*   Updated: 2024/04/07 05:01:31 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/04/07 05:40:04 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTIL_H
 # define UTIL_H
 
-# include <stddef.h>
 # include "env.h"
+# include "minishell.h"
+# include <stddef.h>
 
 char	*ms_strchr(char *str, char c);
 size_t	next_char(char *s, size_t i);
